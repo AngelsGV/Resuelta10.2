0261374027a9b29a21ac4758f24ce1cbc5b31762
AngelsGV <angels.not99@gmail.com>
2024-04-21 19:44:34 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    String texto = "";
+    FileReader in = null;
+    //se declara fuera de la estructura try-catch pera que sea accesible desde fuera
+    try{
+        in = new FileReader("Main.java");
+        int c = in.read();
+        while (c != -1){
+            texto = texto + char c;
+            c = in.read();
         }
+        catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }finally{
+            if (in != null){
+                try{
+                    in.close();
+                }catch (IOException ex){
+                    System.out.println(ex);
+                }
+            }
+        }
+        System.out.println(texto);
     }
+
+
 }
